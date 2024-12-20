9b855498f9591052a73a2b134193e933efea259b
AlexDiEZy <160856024+AlexDiEZy@users.noreply.github.com>
2024-12-20 19:52:59 +0300

    const.py

--- const.py
+++ const.py
@@ -1,4 +1,4 @@
-token_api = "51d0925cf6051fccfda54dbbed569440"
+token_api = "secret_token"
 URI = 'https://www.unlocktool.net/post-in/'
 site_clean = '6Ldg0SMcAAAAACmWTVHNZFTr5B2dRVGv1kivSUXk'
 change_password_page = 'https://www.unlocktool.net/password-change/'
